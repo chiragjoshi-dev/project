ea4a9c41b4abed40abfe314678c02da5dfe42a37
Mikhail Park <packmisha@mail.ru>
2020-12-13 20:45:10 +0300

    added comments and links

--- MikhailPak/js/flip.js
+++ MikhailPak/js/flip.js
@@ -1,19 +1,12 @@
-// var card = document.querySelector('.team__member');
-
-
-// // card.addEventListener('click', function () {
-// //   card.classList.toggle('is-flipped');
-// // });
-
+//flip card function
 function flipCard() {
   $('.team__member.is-flipped').not(this).removeClass('is-flipped');
   $(this).toggleClass('is-flipped');
 }
-
-// card.forEach(()=> card.addEventListener('click', flipCard));
-
+//select all li tags in list
 const cards = document.getElementById('teamList').querySelectorAll('li');
 
+//add function on click for each li
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 });
